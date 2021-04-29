24ecb0029fcf958d1b49c31efc793f67e527f4bb
johnfa9 <jacocella@hotmail.com>
2021-04-29 13:06:34 -0400

    Updated Home Page

---
+++ public_html/heading.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Drum Center World</title>
+    <link rel="stylesheet" href="css/home-styles.css">
+
+</head>
+<body>
+
+<nav>
+    <div class = "home-menu">
+        <a href="index.php"> <img src= "images/drum-center-logo.png" alt = "Drum Center World"></a>
+        <ul>
+            <li><a href="index.php">Home </a></li>
+            <li><a href="Login.php">Login </a></li>
+            <li><a href="Register.php">Register </a></li>
+        </ul>
+    </div>
+</nav>
+
+<div class="homeMenu"></div>
---
+++ public_html/index.php
@@ -0,0 +1,36 @@
+<?php
+ include_once'heading.php';  //navigation menu will be on all web pages
+?>
+
+        <section class="site-description">
+            <h1>Welcome to Drum Center World !</h1>
+         <p>We have the best gear in town</p>
+        </section>
+
+        <section class="categories">
+            <h2>Drum Categories</h2>
+            <div class="drum-category-index"></div>
+                <div>
+                    <h3>
+                        Category 1
+                    </h3>
+                </div>
+                <div>
+                    <h3>
+                        Category 2
+                    </h3>
+                </div>
+                <div>
+                    <h3>
+                        Category 3
+                    </h3>
+                </div>
+                <div>
+                    <h3>
+                        Category 4
+                    </h3>
+                </div>
+            </div>
+        </section>
+    </div>
+</body>
