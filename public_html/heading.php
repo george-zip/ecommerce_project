cf1d6368d65d98d3067a3c4c035d355e5d1e4925
johnfa9 <jacocella@hotmail.com>
2021-05-06 21:58:41 -0400

    Modified navigation, and heading styles

--- public_html/heading.php
+++ public_html/heading.php
@@ -1,25 +1,40 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Drum Center World</title>
-    <link rel="stylesheet" href="css/home-styles.css">
-
+    <link rel="stylesheet" type="text/css" href="css/home-styles.css"/>
 <!--    <link rel="preconnect" href="https://fonts.gstatic.com">-->
-
+</head>
 <body>
-
-<nav>
+<nav id="navigationbar">
     <div>
-        <a href="index.php"> <img src= "images/logo.png" alt = "Drum Center World"></a>
-
         <ul>
-
+            <li><a href="index.php"> <img src= "images/logo.png" alt = "Drum Center World"></a></li>
             <li><a href="index.php">Home </a></li>
-            <li><a href="Login.php">Login </a></li>
-            <li><a href="Register.php">Register </a></li>
+            <?php
+
+
+            if (isset($_SESSION["LoginUser"])){
+                echo  "<li><a href='Login.php'>Login</a></li>";
+                echo  "<li><a href='Logout.php'>Logout</a></li>";
+            }
+            else
+            {
+                echo "<li><a href='Login.php'>Login</a></li>";
+                echo "<li><a href='Register.php'>Register</a></li>";
+            }
+            if (isset($_SESSION["Role"])){
+                if($_SESSION["Role"]==2){ //admin
+                    echo "<li><a href='RegisterEmployee.php'>Employee Administration</a></li>";
+                }
+            }
+            ?>
         </ul>
     </div>
 </nav>
-
 <div class="homeMenu"></div>
